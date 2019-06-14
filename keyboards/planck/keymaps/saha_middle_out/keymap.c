ee06f514dd1de7cab9db2e4412b9d1c2f1066134
mvsaha <mvsaha@users.noreply.github.com>
2019-06-13 21:11:12 -0700

    blah

--- keyboards/planck/keymaps/saha_middle_out/keymap.c
+++ keyboards/planck/keymaps/saha_middle_out/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_ESC,   /****/  KC_DELT,  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P     },
     {  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     _______,  /****/  KC_ENT,   KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN  },
     {  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_BSPC,  /****/  _______,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH  },
-    {  WRKSPCD,  WRKSPCU,  KC_LALT,  KC_LCTL,  KC_SPC,   KC_LGUI,  /****/  KC_RSFT,  SPECIAL,  NUMERIC,  PROGlst,  PROGlst,  PROGnxt  }
+    {  WRKSPCD,  WRKSPCU,  KC_LALT,  KC_LCTL,  KC_SPC,   KC_LGUI,  /****/  NUMERIC,  SPECIAL,  KC_RSFT,  PROGlst,  PROGlst,  PROGnxt  }
 },
 
 [_SPECIAL] = {
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_ESC,   /****/  KC_DELT,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN  },
     {  KC_1,     KC_2,     KC_3,     KC_4,        KC_5,  _______,  /****/  KC_ENT,   KC_6,     KC_7,     KC_8,     KC_9,     KC_0     },
     {  _______,  _______,  _______,  _______,  _______,  KC_BSPC,  /****/  _______,  KC_PIPE,  KC_UNDS,  KC_COMM,  KC_DOT,   KC_SLSH  },
-    {  ___t___,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_SPC,   KC_LGUI,  /****/  KC_RSFT,  _______,  _______,  _______,  _______,  _______  }
+    {  ___t___,  KC_LGUI,  KC_LALT,  KC_LCTL,  KC_SPC,   KC_LGUI,  /****/  ___t___,  KC_RSFT,  _______,  _______,  _______,  _______  }
 }
 
 };
